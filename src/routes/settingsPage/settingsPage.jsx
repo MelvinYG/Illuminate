aba1975d4942f7053ff4250a6d240ff4c607a4c6
MelvinYG <melvingeorge204@gmail.com>
2024-10-25 10:12:32 +0530

    feat: Dark and Light mode

--- src/routes/settingsPage/settingsPage.jsx
+++ src/routes/settingsPage/settingsPage.jsx
@@ -1,10 +1,47 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext'; 
 import './settingsPage.css';
 
-
 const SettingsPage = () => {
-  return (
-    <div>settingsPage</div>
-  )
-}
+    const { darkMode, toggleDarkMode } = useContext(AuthContext);
 
-export default SettingsPage
+    const handleModeChange = (e) => {
+        const isDarkMode = e.target.value === 'Dark';
+        toggleDarkMode(isDarkMode);
+    };
+
+    return (
+        <div className='settings-page'>
+            <div className="settings-details">
+                <h2>Settings</h2>
+                <div className="general">
+                    <h2>General</h2>
+                    <div className="preference">
+                        <form className='flex gap-4'>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="Dark"
+                                    checked={darkMode === true}
+                                    onChange={handleModeChange}
+                                />
+                                Dark
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="Light"
+                                    checked={darkMode === false}
+                                    onChange={handleModeChange}
+                                />
+                                Light
+                            </label>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SettingsPage;
